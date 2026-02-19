a96767c3a6e65ba71cd05ab6eb3ae33343571e1a
NikoFan <neoleg2005@gmail.com>
2026-02-19 07:47:16 +0300

    Создание стартового окна

--- TaskTracker_KR/obj/Debug/net8.0-windows/TaskTracker_KR.AssemblyInfo.cs
+++ TaskTracker_KR/obj/Debug/net8.0-windows/TaskTracker_KR.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskTracker_KR")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf8eadf20ac10ecc6c08268b7858b23b33954981")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e1d4d5424350d25af103cacea6891393ee67713")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskTracker_KR")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskTracker_KR")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
