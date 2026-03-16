97f09e1de3013ad765f0731c507aef002df4c0da
NikoFan <neoleg2005@gmail.com>
2026-03-16 09:53:03 +0300

    Создана модель таблицы Tasks
    доработать:
    - Проверить работу запросов

---
+++ TaskTracker_KR/Models/Task.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Supabase.Postgrest.Attributes;
+using Supabase.Postgrest.Models;
+
+namespace TaskTracker_KR.Models
+{
+    [Table("Tasks")]
+    public class Task : BaseModel
+    {
+        [Column("task_id")]
+        public int Id { get; set; }
+
+        [Column("task_title")]
+        public string? Title { get; set; } = string.Empty;
+
+        [Column("task_text")]
+        public string? Text { get; set; } = string.Empty;
+
+        [Column("task_create_date")]
+        public DateOnly CreateDate { get; set; }
+
+        [Column("task_end_date")]
+        public DateOnly EndDate { get; set; }
+
+        [Column("task_end_fact_date")]
+        public DateOnly FactEndDate { get; set; }
+
+        [Column("task_status")]
+        public bool Status { get; set; }
+
+        [Column("account_employee_FK")]
+        public int EmployeeFK { get; set; }
+        public Account? Account { get; set; }
+
+
+    }
+}
